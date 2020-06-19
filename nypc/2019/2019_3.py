aa21338e6a5574fe9b104b97d4eeaf9db6d7b788
Song Kitae <40349572+kitae0522@users.noreply.github.com>
2020-06-19 13:02:49 +0900

    Last Update 200619

---
+++ nypc/2019/2019_3.py
@@ -0,0 +1,18 @@
+if __name__ == "__main__":
+    start_hp, count = map(int, input().split())
+    max_hp = start_hp
+    for i in range(count):
+        log, H = map(int, input().split())
+        if log == 1:
+            if start_hp < 0:
+                start_hp = 0
+            else:
+                start_hp -= H
+        elif log == 2:
+            if start_hp + H >= max_hp:
+                start_hp += H
+        elif log == 3:
+            start_hp += H
+            if start_hp > max_hp:
+                max_hp = start_hp
+    print(max_hp)
