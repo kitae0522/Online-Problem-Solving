8b8cf56b5e2b8e8ae091a7b724b82c5ed947543f
Song Kitae <40349572+kitae0522@users.noreply.github.com>
2020-06-20 13:22:22 +0900

    Last Update 200620

---
+++ nypc/2019/2019_4.py
@@ -0,0 +1,12 @@
+"""
+문제 링크 : https://nypc.github.io/2019/2019_online_4.html
+
+code by Song Kitae(DKSH)
+"""
+
+if __name__ == "__main__":
+    count = int(input())
+    N1 = [int(x) for x in input().strip().split()]
+    N2 = [int(y) for y in input().strip().split()]
+    print(N1)
+    print(N2)
