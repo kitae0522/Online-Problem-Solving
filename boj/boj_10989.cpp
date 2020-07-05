4f929a702a79fb1db42be487fc6d3af096304239
Song Kitae <40349572+kitae0522@users.noreply.github.com>
2020-07-05 14:35:55 +0900

    Update

---
+++ boj/boj_10989.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int arr[10000];
+int main() {
+    int N;
+    scanf("%d", &N);
+    for(int i=0;i<N;i++) {
+        int x;
+        scanf("%d", &x);
+        arr[x-1]++;
+    }
+    for(int i=0;i<10000;i++) {
+        if(arr[i] > 0) {
+            for(int j=0;j<arr[i];j++) {
+                printf("%d\n", i+1);
+            }
+        }
+    }
+    return 0;
+}
