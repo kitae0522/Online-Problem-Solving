c58294848dbe7ce01a973767c957e889fc2e01ef
Song Kitae <40349572+kitae0522@users.noreply.github.com>
2020-07-13 09:18:20 +0900

    Update

---
+++ boj/boj_14563.py
@@ -0,0 +1,20 @@
+def perfect_num(num):
+    sum = 0
+    for i in range(1, num, 1):
+        if num % i == 0:
+            sum += i
+    if sum == num:
+        print("Perfect")
+    elif sum < num:
+        print("Deficient")
+    else:
+        print("Abundant")
+
+if __name__ == "__main__":
+    def main():
+        test_case_count = int(input())
+        test_num_list = list(map(int, input().split()))
+        for i in range(test_case_count):
+            perfect_num(test_num_list[i])
+
+    main()
