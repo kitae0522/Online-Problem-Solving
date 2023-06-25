f0140d42c9a1ecb167331049e44c54d6b285fd5a
Ted Song <kitae040522@gmail.com>
2023-06-26 01:14:26 +0900

    update

---
+++ boj/10699.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+#define STDIO() ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+int main()
+{
+    STDIO();
+    cout << "2023-06-26";
+    return 0;
+}
